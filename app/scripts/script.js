75ed9f95103a3ac479b9e6598f6077a38c18aa6b
zivepstein <zge02013@mymail.pomona.edu>
2015-04-11 04:19:41 -0700

    added img

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
 	var clicked = false;
-
-	$('.btn').click(function(){	
-	 	if (!clicked){
-	 		clicked = true;
+	$('.title').click(function(){	
+	 	if (clicked){
+	 		clicked = false;
+	 		
 	 		$( "#controls" ).animate({
-    			marginTop: "-=338",
-  			}, 500, function() {
+    			marginTop: "+=320",
+  			}, 700, function() {
    			 // Animation complete.
   			});
+  			setTimeout(function() {
+  				$( '.img' ).fadeTo( "medium", 1);
+  			},350);
 	 	}
 	}); 
+	$('.btn').click(function(){	
+	 	if (!clicked){
+	 		
+	 		clicked = true;
+	 		$( "#controls" ).animate({
+    			marginTop: "-=320",
+  			}, 700, function() {
+   			 // Animation complete.
+  			});
+  			 $( '.img' ).fadeTo( "medium", 0 );
+	 	}
+	}); 
+
 });
