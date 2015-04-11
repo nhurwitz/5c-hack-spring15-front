0a4d0075da416f1544fc8e3d590e2bef9e60bf83
Dima Smirnov <dimkasmir@gmail.com>
2015-04-11 06:25:59 -0700

    added timer

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -16,16 +16,22 @@ $(document).ready(function() {
   			setTimeout(function() {
   				$( '.img' ).fadeTo( "medium", 1);
   			},350);
-  			$("#graph").remove();
-			$("#graphrow").append("<div id='graph'></div>");
-			cy_init();
+  			
+			
+  				$("#graph").remove();
+				$("#graphrow").append("<div id='graph'></div>");
+				cy_init();
+  			
 	 	}
 	}); 
 	$('.btn').click(function(){	
 		var a = $("#start").val();
 		var b = $("#end").val();
 		if ($(this).attr('name') == "fsp"){
-			draw(["banana", "Paris"]);
+			setTimeout(function() {
+  				draw(["banana", "Paris"]);
+  			},500);
+			
 		} else{ 
 		if ($(this).attr('name') == "ifl" ){
 			var a = document.getElementById("start");
